8b80ffd6869817b4578080dac08eab9fd24a9138
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2021-04-24 20:04:07 -0300

    feat: ThreadPool struct and new constructor

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::net::TcpStream;
 use std::fs;
 use std::thread;
 use std::time::Duration;
+use webserver::ThreadPool;
 
 fn main() {
   let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
